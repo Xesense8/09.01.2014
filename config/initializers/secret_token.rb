4879e9683c73fb1dc310aa4bddf82ad796301f72
ion <ion@local.85411.com>
2014-03-08 19:36:12 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoblog::Application.config.secret_key_base = '86dfd01e68af8cfe92ee656c5742305f77904741defef941313a7bbea8d6c2f8641f19b1b5e833ce12fbff474e98cd5bd188bb45a7f77beaad373d29819d0ffb'
